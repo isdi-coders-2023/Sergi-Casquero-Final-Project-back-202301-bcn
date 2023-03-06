d16396a7f17872244960dce664784a7ddb49df9d
Sergi Casquero <sergicasquero@gmail.com>
2023-03-06 16:52:19 +0100

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+
+  extends: ["xo", "prettier"],
+  overrides: [
+    {
+      extends: ["xo-typescript", "prettier"],
+      files: ["*.ts", "*.tsx"],
+    },
+    {
+      files: ["src/**/models/**/*.ts", "src/server/routers/usersRouters.ts"],
+      rules: { "@typescript-eslint/naming-convention": "off" },
+    },
+    {
+      rules: {
+        "@typescript-eslint/consistent-type-definitions": [
+          "error",
+          "interface",
+        ],
+        "@typescript-eslint/consistent-type-assertions": ["off"],
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+};
---
+++ jest.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/src/**/*.test.ts"],
+  resolver: "jest-ts-webcompat-resolver",
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/index.ts",
+    "!src/react-app-env.d.ts",
+    "!src/setupTests.ts",
+  ],
+};
