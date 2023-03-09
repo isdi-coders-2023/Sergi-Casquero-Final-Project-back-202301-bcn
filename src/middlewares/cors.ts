51f44f56dee7c264f1e9d7932a6894ecc12d3355
Sergi Casquero <sergicasquero@gmail.com>
2023-03-09 15:17:20 +0100

    Add cors allowed origins

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,6 +1,8 @@
 import type cors from "cors";
 
-const allowedOrigins = ["http://localhost:5000"];
+const netlifyUrl = process.env.NETLIFY_URL!;
+
+const allowedOrigins = [`http://localhost:3000`, netlifyUrl];
 
 const options: cors.CorsOptions = {
   origin: allowedOrigins,
