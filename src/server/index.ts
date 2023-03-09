ccb596ad8656163d4bd915012b6d3be0ab64018c
Sergi Casquero <sergicasquero@gmail.com>
2023-03-09 15:16:25 +0100

    Fix errorMiddlewares register and loadEnvironment import

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,6 +3,10 @@ import morgan from "morgan";
 import cors from "cors";
 import options from "../middlewares/cors.js";
 import userRouter from "./routers/userRouter.js";
+import {
+  generalError,
+  notFoundError,
+} from "../middlewares/errorMiddlewares/errorMiddlewares.js";
 
 const app = express();
 
@@ -13,5 +17,8 @@ app.use(morgan("dev"));
 app.use(cors(options));
 
 app.use("/user", userRouter);
+
+app.use(notFoundError);
+app.use(generalError);
 
 export default app;
