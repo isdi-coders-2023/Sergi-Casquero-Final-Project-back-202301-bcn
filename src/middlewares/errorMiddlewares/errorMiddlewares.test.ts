e4b302d5873eb696823c71f725ed752ad960dd16
Sergi Casquero <sergicasquero@gmail.com>
2023-03-09 15:40:36 +0100

    Fix type assertions in errorMiddlewares tests

--- src/middlewares/errorMiddlewares/errorMiddlewares.test.ts
+++ src/middlewares/errorMiddlewares/errorMiddlewares.test.ts
@@ -2,11 +2,11 @@ import { type NextFunction, type Request, type Response } from "express";
 import { CustomError } from "../../CustomError/CustomError.js";
 import { generalError, notFoundError } from "./errorMiddlewares";
 
-const res = {
+const res: Partial<Response> = {
   status: jest.fn().mockReturnThis(),
   json: jest.fn(),
-} as Partial<Response>;
-const req = {} as Request;
+};
+const req: Partial<Request> = {};
 const next = jest.fn() as NextFunction;
 
 beforeEach(() => jest.clearAllMocks());
@@ -14,7 +14,7 @@ beforeEach(() => jest.clearAllMocks());
 describe("Given a notFoundError middleware", () => {
   describe("When it receives a request", () => {
     test("Then it should call its next method", () => {
-      notFoundError(req, res as Response, next);
+      notFoundError(req as Request, res as Response, next);
 
       expect(next).toHaveBeenCalled();
     });
@@ -31,7 +31,7 @@ describe("Given a generalError middleware", () => {
         "Something went wrong"
       );
 
-      generalError(error, req, res as Response, next);
+      generalError(error, req as Request, res as Response, next);
 
       expect(res.status).toHaveBeenCalledWith(statusCode);
     });
@@ -43,7 +43,7 @@ describe("Given a generalError middleware", () => {
       const expectedStatusCode = 500;
       const expectedMessage = { error: "Something went wrong" };
 
-      generalError(error, req, res as Response, next);
+      generalError(error, req as Request, res as Response, next);
 
       expect(res.status).toHaveBeenCalledWith(expectedStatusCode);
       expect(res.json).toHaveBeenCalledWith(expectedMessage);
